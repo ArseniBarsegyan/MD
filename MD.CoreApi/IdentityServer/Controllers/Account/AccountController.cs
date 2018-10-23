7f675396b794017d471ed497c19b14a40ecaa851
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-10-23 21:45:28 +0300

    Styles for IdentityServer updated.

--- MD.CoreApi/IdentityServer/Controllers/Account/AccountController.cs
+++ MD.CoreApi/IdentityServer/Controllers/Account/AccountController.cs
@@ -145,61 +145,6 @@ namespace IdentityServer.Controllers.Account
             return View(vm);
         }
 
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new AppUser
-                {
-                    Email = model.Email,
-                    UserName = model.Email
-                };
-                // Adding user
-                var result = await _userManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    // Set cookies
-                    await _signInManager.SignInAsync(user, false);
-                    return RedirectToAction("Index", "Home");
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
-            return View(model);
-        }
-
-        //[HttpPost]
-        //public async Task<IActionResult> Register(RegisterViewModel model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    var user = new AppUser
-        //    {
-        //        UserName = model.Email,
-        //        Email = model.Email
-        //    };
-        //    var result = await _userManager.CreateAsync(user, model.Password);
-
-        //    if (result.Succeeded)
-        //    {
-        //        await _signInManager.SignInAsync(user, false);
-        //        return Ok();
-        //    }
-        //    return BadRequest();
-        //}
-
         /// <summary>
         /// Show logout page
         /// </summary>
