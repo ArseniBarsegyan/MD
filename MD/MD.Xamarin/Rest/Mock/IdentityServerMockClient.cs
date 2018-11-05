736deddcbbdfcadf5e37403ad957e3a1341a8ae6
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-11-06 00:28:34 +0300

    Add IdentityServerMockClient

---
+++ MD/MD.Xamarin/Rest/Mock/IdentityServerMockClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace MD.Xamarin.Rest.Mock
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Fake implementation of <see cref="T:MD.Xamarin.Rest.IIdentityServerClient" />
+    /// </summary>
+    public class IdentityServerMockClient : IIdentityServerClient
+    {
+        /// <summary>
+        /// Return fake token.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<TokenResponse> GetToken(string username, string password)
+        {
+            return await Task.FromResult(new TokenResponse("success"));
+        }
+    }
+}
