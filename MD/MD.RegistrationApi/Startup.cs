2413719898b55640e4d74fb43c586a7abf7f803b
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-11-23 17:49:25 +0300

    RegistrationApi, XamarainTestApi: added swagger

--- MD/MD.RegistrationApi/Startup.cs
+++ MD/MD.RegistrationApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MD.RegistrationApi
 {
@@ -45,6 +46,11 @@ namespace MD.RegistrationApi
                 .AddDefaultTokenProviders();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MD Registration API", Version = "v.0.1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +68,11 @@ namespace MD.RegistrationApi
             app.UseCors(ConstantsHelper.CorsPolicy);
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MD Registration API V.0.1");
+            });
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
--- MD/MD.XamarinTestApi/Startup.cs
+++ MD/MD.XamarinTestApi/Startup.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MD.Data;
+﻿using MD.Data;
 using MD.Helpers;
 using MD.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MD.XamarinTestApi
 {
@@ -59,6 +53,11 @@ namespace MD.XamarinTestApi
                 .AddDefaultTokenProviders();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MD Core API", Version = "v.0.1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +75,11 @@ namespace MD.XamarinTestApi
             app.UseCors(ConstantsHelper.CorsPolicy);
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MD API V.0.1");
+            });
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
